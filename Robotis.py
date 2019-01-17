d10232d491e692186b1a29d35f6a50d385551501
javad <javadghoddousi81@gmail.com>
2019-01-17 00:14:16 -0500

    fix

--- Robotis.py
+++ Robotis.py
@@ -4,10 +4,13 @@ from Methods import *
 excel_file = "Book1.xlsx"
 
 #dxl.enable_torque(fids)
+mm=motion(excel_file)[1]
+set_pos(mm)
+
 while True:
     print(get_pos())
-    dxl.disable_torque(fids)
     sleep(1)
+    dxl.disable_torque(fids)
 #    for i in range(1,12):
 #        mm=motion(excel_file)[i]
 #        set_pos(mm)
