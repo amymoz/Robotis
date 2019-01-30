7c0e4fb0f7334f2b61bb5678c95c7e432d876bdf
javad <javadghoddousi81@google.com>
2019-01-31 01:13:48 +0330

    added

---
+++ Methods.py
@@ -0,0 +1,64 @@
+from time import sleep 
+import xlrd
+from os import system
+import pypot.dynamixel as dynamixel
+
+def prt(st,end='\n'):
+    system("printf \"{0}\"".format(st+end))
+
+def set_speed(speed):
+    dicts={}
+    for i in range(0,len(fids)):
+        dicts[fids[i]]=speed
+    dxl.set_moving_speed(dicts)
+
+def set_pos(poses):
+    dicts={}
+    for i in range(0,len(fids)):
+        dicts[fids[i]]=poses[i]
+    dxl.set_goal_position(dicts)
+
+def get_pos():
+    return dxl.get_present_position(tuple(fids)) #Tuple
+
+def motion_excel(addr):
+    WorkBook = xlrd.open_workbook(addr)
+    sheet = WorkBook.sheet_by_index(0)
+    amotion = []
+    angles = []
+    for a in range(sheet.nrows):
+        for b in range(sheet.ncols):
+            meghdr = sheet.cell_value(a, b)
+            angles.append(meghdr)
+        amotion.append(angles)
+        angles = []
+    return amotion
+def motion_file(addr):
+    file = open(addr,'r')
+    data = file.read()
+    file.close()
+    amotion = []
+    frms = []
+    frm = []
+    for a in data.split('\n'):
+        str_frms = str(a).split(';')
+        for b in range(1,len(str_frms)):
+            str_frm = str(str_frms[b]).split(',')
+            for c in range(len(str_frm)):
+                frm.append(str_frm[c])
+            frms.append(frm)
+            frm = []
+        
+        amotion.append(frms)
+        frms = []
+    return amotion
+def play_frames(file_frms,selected_frms):
+    for a in selected_frms:
+        for b in file_frms[a]:
+                set_pos(b)
+                sleep(0.03) #to be continued
+
+#Start Dynamixel
+port = dynamixel.get_available_ports()[0]
+dxl = dynamixel.DxlIO(port)
+fids = dxl.scan(ids=list(range(19)))
---
+++ Robotis.py
@@ -0,0 +1,94 @@
+from os import system
+from time import sleep
+import cv2
+from Methods import *
+
+poss_file = "/media/Game/Professional/Project/GitArch/Robotis/poss.txt"
+frames = motion_file(poss_file)
+dxl.enable_torque(fids)
+
+set_speed(200)
+#poss=[8,15,15,79,51,58,58,65,37,44,44,79]
+#for i in poss[0:4]:
+#    set_pos(frames[i+1])
+#    sleep(0.7)
+#while True:
+#    for i in poss[4:]:
+#        set_pos(frames[i+1])
+#        sleep(0.3)
+
+cv2.namedWindow('window')
+dicts={}
+for i in range(0,len(fids)):
+    dicts[fids[i]]=True
+dxl.set_force_control_enable(dicts)
+
+fid=str(fids[0])
+pos=0
+np=''
+def tec():
+    dxl.set_goal_position({int(fid):pos})
+def home():
+    system("clear")
+    print(fids)
+while True:
+    home()
+    while True :
+        pos=float(get_pos()[(int(fid)-1)])
+        prt("\033[2K\033[0Gchange value with [w](Up) [s](Down) for servo( {} : {} ) or enter degree... then press enter ==> {}".format(fid,pos,np),end='')
+        key=cv2.waitKey(0)
+        if (key < 58 and key > 47):
+            np += chr(int(key))
+        elif (key == 8):
+            np = np[:len(np)-1]
+        elif (key == ord('-')):
+            np = '-'
+        elif (key == ord('d')):
+            print('')
+            while True:
+                prt("\033[2K\033[0GInter servo id==> {}".format(fid),end='')
+                out = cv2.waitKey(0)
+                if (out < 58 and out > 47):
+                    fid += str(int(out)-48)
+                elif (out == 8):
+                    fid = fid[:len(fid)-1]
+                elif (out == 13):
+                    break
+            home()
+        elif (key == ord('f')):
+            frm=''
+            print('')
+            while True:
+                prt("\033[2K\033[0GInter frame id==> {}".format(frm),end='')
+                out = cv2.waitKeyEx(0)
+                if (out < 58 and out > 47):
+                    frm += chr(int(out))
+                elif (out == ord(',')):
+                    frm += ','
+                elif (out == 8):
+                    frm = frm[:len(frm)-1]
+                elif (out == 13):
+                    break
+            frm = str(frm).split(',')
+            for i in range(len(frm)):
+                frm[i] = int(frm[i])-1
+            set_pos(frames[frm[0]][frm[1]])
+            home()
+        elif (key == ord('p')):
+            while True:
+                play_frames(frames,[39])
+                sleep(1)
+                
+        elif (key == ord('w')):
+            pos += float(2)
+            tec()
+        elif (key == ord('s')):
+            pos -= float(2)
+            tec()
+        elif (key == 13 and np != ''):
+            pos = float(np)
+            tec()
+            np=''
+        elif (key == 13):
+            break
+dxl.disable_torque(fids)
